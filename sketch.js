1cf1acc8f88f7e84043fd9f45d0edc60b2e267db
Swaraj1233 <75794477+Swaraj1233@users.noreply.github.com>
2021-04-22 19:55:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var canvas;
+var gameState = 0;
+var contestantCount,question,contestant,quiz,answer,database,allContestants;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
